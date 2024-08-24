cddd9743d2e40e0d78ea22b943217d17d4b74b0a
andomain <sam@andomain.co.uk>
2024-08-25 00:09:42 +0100

    Add Talk/Conference models/Factories etc.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -803,7 +803,7 @@
                 class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
             >
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid items-center grid-cols-2 gap-2 py-10 lg:grid-cols-3">
+                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:col-start-2 lg:justify-center">
                             <svg
                                 class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]"
@@ -818,7 +818,7 @@
                             </svg>
                         </div>
                         @if (Route::has('login'))
-                            <nav class="flex justify-end flex-1 -mx-3">
+                            <nav class="-mx-3 flex flex-1 justify-end">
                                 @auth
                                     <a
                                         href="{{ url('/dashboard') }}"
@@ -854,7 +854,7 @@
                                 id="docs-card"
                                 class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
-                                <div id="screenshot-container" class="relative flex items-stretch flex-1 w-full">
+                                <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
                                     <img
                                         src="https://laravel.com/assets/img/welcome/docs-light.svg"
                                         alt="Laravel documentation screenshot"
@@ -1134,7 +1134,7 @@
                         </div>
                     </main>
 
-                    <footer class="py-16 text-sm text-center text-black dark:text-white/70">
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
                 </div>
